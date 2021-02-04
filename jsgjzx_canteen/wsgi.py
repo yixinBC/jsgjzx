34e39829b11be2a89952d3f3f9cf08f560802d1b
yixinBC <buyixin2003@qq.com>
2021-02-04 21:22:38 +0800

    change sth. but failed

--- jsgjzx_canteen/wsgi.py
+++ jsgjzx_canteen/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for jsgjzx_canteen project.
+WSGI config for jsgjzx project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jsgjzx_canteen.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jsgjzx.settings')
 
 application = get_wsgi_application()
