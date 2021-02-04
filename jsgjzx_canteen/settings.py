a83cd46719f410c3d4d3bdb1194b4168ae0b35e8
yixinBC <61578883+yixinBC@users.noreply.github.com>
2021-02-04 21:11:10 +0800

    Update settings.py

--- jsgjzx_canteen/settings.py
+++ jsgjzx_canteen/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'ch-Hans'
+LANGUAGE_CODE = 'zh-hans'
 
 TIME_ZONE = 'Asia/Shanghai'
 
