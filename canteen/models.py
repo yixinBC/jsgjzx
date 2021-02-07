7b66f5b4255afc90933249a050c66488fa036c03
yixinBC <buyixin2003@qq.com>
2021-02-07 18:38:47 +0800

    start to program the model

--- canteen/models.py
+++ canteen/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
+
 # Create your models here.
+class Class(models.Model):
+    class_id = models.CharField(max_length=4)
+
+    def __str__(self) -> str:
+        return f"班级编号：{self.class_id}"
+
+
+class Student(models.Model):
+    student_class = models.ForeignKey(Class, verbose_name="班级", on_delete=models.CASCADE)
+    student_id = models.CharField(verbose_name="学号", max_length=6)
+    name = models.CharField(verbose_name="姓名", max_length=16)
+    password = models.CharField(verbose_name="密码—md5加密", max_length=32)
+
+    def __str__(self) -> str:
+        return f"{self.student_id} {self.name}"
+
+
+class Meal(models.Model):
+    date = models.DateField(verbose_name="日期")
+    index = models.PositiveSmallIntegerField(verbose_name="索引")
+    description = models.CharField(verbose_name="描述", max_length=4)
+
+    def __str__(self) -> str:
+        return f"{self.date}{self.description}"
+
+    def __lt__(self, other) -> bool:
+        if self.date < other.date:
+            return True
+        elif self.date == other.date and self.index < other.index:
+            return True
+        else:
+            return False
+
+    def __gt__(self, other) -> bool:
+        return not self < other
+
+    def __eq__(self, other) -> bool:
+        return self.date == other.date and self.index == other.index
+
+    def __le__(self, other) -> bool:
+        return self < other or self == other
+
+    def __ge__(self, other) -> bool:
+        return self > other or self == other
