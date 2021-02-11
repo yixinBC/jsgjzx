8b2438432c4bb05bad45d92622f6cdee879efd46
yixinBC <buyixin2003@qq.com>
2021-02-11 15:36:06 +0800

    start to program the view

--- jsgjzx/urls.py
+++ jsgjzx/urls.py
@@ -19,6 +19,7 @@ from . import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('canteen/', include('canteen.urls')),
     path('admin/', admin.site.urls),
     * static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
     path('', include('index.urls'))
