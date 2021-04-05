331461d0604f2ea04a487c135982f63b869a5b07
yixinBC <yixinbc@firefox.com>
2021-04-05 17:30:48 +0800

    add activity app

--- jsgjzx/urls.py
+++ jsgjzx/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('canteen/', include('canteen.urls')),
+    path('activity/', include('activity.urls')),
     path('admin/', admin.site.urls),
     * static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
     path('', include('index.urls'))
